050e746279d2a3b8b33f62dbe8aa7b075a8c9ee4
SWelbeck <swelbeck12@gmail.com>
2024-10-09 22:57:58 -0400

    working to render image

--- server.mjs
+++ server.mjs
@@ -15,10 +15,16 @@ app.set("view engine", "pug");
 // Set the views directory
 app.set("views", "./views");
 
-// serve static files
+// Serve static files
 app.use(express.static("./images"));
 
-// error handler
+// Middleware
+app.use((req, res, next) => {
+  console.log(`Request Method: ${req.method}, URL: ${req.url}`);
+  next();
+});
+
+// Error handler
 app.use((err, req, res, next) => {
   res.status(400).send(err.message);
 });
